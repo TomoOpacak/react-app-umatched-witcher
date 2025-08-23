0d899d3ba449e196c23313b5e484ceb5e09ba186
Tomo Opacak <reattera321@gmail.com>
2025-08-23 09:25:49 +0200

    new heros

--- src/components/CharacterData.js
+++ src/components/CharacterData.js
@@ -9,6 +9,17 @@ import triss_yen from "../assets/images/triss_yen_hero.webp";
 import yen from "../assets/images/yen.webp";
 import triss from "../assets/images/triss.webp";
 import eredin from "../assets/images/eredin_hero.webp";
+import alisa from "../assets/images/alice_hero.webp";
+import jabberwock from "../assets/images/jabberwock_side.webp";
+import littlered from "../assets/images/little_red_hero.webp";
+import hunter from "../assets/images/hunter_side.webp";
+import upcoming from "../assets/images/upcoming_hero.webp";
+import robin from "../assets/images/robin_hero.webp";
+import achiles from "../assets/images/achiles_hero.webp";
+import medusa from "../assets/images/medusa_hero.webp";
+import arthur from "../assets/images/arthur_hero.webp";
+import sherlock from "../assets/images/sherlock_hero.webp";
+import dracula from "../assets/images/dracula_hero.webp";
 
 const characters = [
   {
@@ -120,6 +131,189 @@ const characters = [
       },
     ],
     sidekicks: [],
+  },
+  {
+    id: 7,
+    name: "Alisa",
+    image: alisa,
+    heroes: [
+      {
+        name: "Alisa",
+        type: "melee",
+        image: alisa,
+        hp: 13,
+        color: "pink",
+      },
+    ],
+    sidekicks: [
+      {
+        name: "Jabberwock",
+        type: "melee",
+        image: jabberwock,
+        hp: 8,
+        color: "black",
+      },
+    ],
+  },
+  {
+    id: 8,
+    name: "Crvenkapica",
+    image: littlered,
+    heroes: [
+      {
+        name: "Crvenkapica",
+        type: "melee",
+        image: littlered,
+        hp: 14,
+        color: "#ad0808ff",
+      },
+    ],
+    sidekicks: [
+      {
+        name: "Lovac",
+        type: "ranged",
+        image: hunter,
+        hp: 9,
+        color: "silver",
+      },
+    ],
+  },
+  {
+    id: 9,
+    name: "Robin Hood",
+    image: robin,
+    heroes: [
+      {
+        name: "Robin Hood",
+        type: "ranged",
+        image: robin,
+        hp: 13,
+        color: "green",
+      },
+    ],
+    sidekicks: [],
+  },
+  {
+    id: 10,
+    name: "Ahilej",
+    image: achiles,
+    heroes: [
+      {
+        name: "Ahilej",
+        type: "melee",
+        image: achiles,
+        hp: 18,
+        color: "#faf06aff",
+      },
+    ],
+    sidekicks: [
+      {
+        name: "Patroklo",
+        type: "melee",
+        image: upcoming,
+        hp: 6,
+        color: "#b08d57",
+      },
+    ],
+  },
+  {
+    id: 11,
+    name: "Meduza",
+    image: medusa,
+    heroes: [
+      {
+        name: "Dolazi Uskoro",
+        type: "ranged",
+        image: medusa,
+        hp: 16,
+        color: "#57b072ff",
+      },
+    ],
+    sidekicks: [],
+  },
+  {
+    id: 12,
+    name: "Kralj Arhtur",
+    image: arthur,
+    heroes: [
+      {
+        name: "Kralj Arhtur",
+        type: "melee",
+        image: arthur,
+        hp: 18,
+        color: "#b05757ff",
+      },
+    ],
+    sidekicks: [
+      {
+        name: "Merlin",
+        type: "ranged",
+        image: upcoming,
+        hp: 7,
+        color: "#b05757ff",
+      },
+    ],
+  },
+  {
+    id: 13,
+    name: "Sherlock Holmes",
+    image: sherlock,
+    heroes: [
+      {
+        name: "Sherlock Holmes",
+        type: "melee",
+        image: sherlock,
+        hp: 16,
+        color: "#b8a852ff",
+      },
+    ],
+    sidekicks: [
+      {
+        name: "Dr. Watson",
+        type: "ranged",
+        image: upcoming,
+        hp: 8,
+        color: "grey",
+      },
+    ],
+  },
+  {
+    id: 14,
+    name: "Dracula",
+    image: dracula,
+    heroes: [
+      {
+        name: "Dracula",
+        type: "melee",
+        image: dracula,
+        hp: 13,
+        color: "black",
+      },
+    ],
+    sidekicks: [],
+  },
+  {
+    id: 15,
+    name: "Dolazi Uskoro",
+    image: upcoming,
+    heroes: [
+      {
+        name: "Dolazi Uskoro",
+        type: "melee",
+        image: upcoming,
+        hp: 100,
+        color: "grey",
+      },
+    ],
+    sidekicks: [
+      {
+        name: "Dolazi Uskoro",
+        type: "melee",
+        image: upcoming,
+        hp: 100,
+        color: "grey",
+      },
+    ],
   },
 ];
 export default characters;
